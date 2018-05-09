022c2e44420ef2dfbb4a75c4503bc9460658f068
yanasavva <yana1579@yandex.ru>
2018-05-09 20:56:57 +0300

    data processing + bot corpus

--- bot.py
+++ bot.py
@@ -1,12 +1,22 @@
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from vectorize_sentences import Mes2Vec
-from datasets.twitter.data import clear_message
+from data.data import clear_message
 from random import randint
 
 
-updater = Updater(token='TOKEN')
+updater = Updater(token='494392101:AAGM1SLC5ImvjzdYiWQpwgWq54NjpdzbsMg')
 dispatcher = updater.dispatcher
+sticker_ids = ['CAADAgADAQADFgqsEHouQiu0Tr5WAg', 'CAADAgADBwADFgqsEA7M9oZNeed8Ag',
+               'CAADAgADEQADFgqsEG1HufYsO4V5Ag', 'CAADAgADEAADFgqsELpgOnM_DpG7Ag',
+               'CAADAgADFQADFgqsEFLrgRpOhlphAg', 'CAADBAADjAQAAsY_ZAWFy4Pje1d7uwI',
+               'CAADBAADegQAAsY_ZAWk7WGNTDlmKgI', 'CAADBAADtAQAAsY_ZAVabSOrYYSmmwI',
+               'CAADBAADBAUAAsY_ZAXaOdWMZTPf6gI', 'CAADBAADLggAAsY_ZAUwPrz5kv3YTQI',
+               'CAADAgADRgIAAiCBFQABMX1nLcHbCRIC', 'CAADAgADTgIAAiCBFQABmcFR_vyclDcC',
+               'CAADAgADXgIAAiCBFQAByI7ks8Gg9LAC', 'CAADAgADZgIAAiCBFQABU8uIJXg5VzgC',
+               'CAADAQAD3QAD_QqQBPBu5mpVRbTvAg', 'CAADAQADMAEAAv0KkATFZWROq-cVRgI',
+               'CAADAgADVgEAAsSraAsIwV4DAgM1agI', 'CAADAgADNAEAAsSraAsSav54wPzlQgI',
+               'CAADAgADaAEAAsSraAvIKFzEIsyHQgI']
 
 
 def start(bot, update):
@@ -14,26 +24,51 @@ def start(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Привет!')
 
 
-def answere(bot, update):
+def answer(bot, update):
     question = update.message.text
     response = m2v.throw_sentence(clear_message(question))
     bot.send_message(chat_id=update.message.chat_id, text=response)
 
-def audio(bot, update):
-    print("got audio")
-    responds=['сорь, не могу послушать','ага, щаз бы на паре аудио слушать','блин, наушники не могу найти','потом послушаю','я занята, не могу послушать']
+def voice(bot, update):
+    responds=['сорь, не могу послушать','ага, щаc бы на паре аудио слушать','блин, наушники не могу найти',
+              'потом послушаю','я занята, не могу послушать']
     bot.send_message(chat_id=update.message.chat_id, text=responds[randint(0,len(responds) - 1)])
+
+
+def location(bot, update):
+    responds=['уже бегу к тебе','ну я ведь вссе равно потеряюсь','супер, там и встретимся']
+    bot.send_message(chat_id=update.message.chat_id, text=responds[randint(0,len(responds) - 1)])
+
+
+def photo(bot, update):
+    responds=['но... у меня ведь нет глаз...','я пока не шарю в CNN и picture segmentation :(']
+    bot.send_message(chat_id=update.message.chat_id, text=responds[randint(0,len(responds) - 1)])
+    bot.send_sticker()
+
+
+def document(bot, update):
+    responds=['ты лучше просто поговори со мной','не, такое пока не спарщу, давай словами','я понимаю только русские слова','а можно словами?']
+    bot.send_message(chat_id=update.message.chat_id, text=responds[randint(0,len(responds) - 1)])
+
+
+def sticker(bot, update):
+    bot.send_sticker(chat_id=update.message.chat_id, sticker=sticker_ids[randint(0,len(sticker_ids) - 1)])
 
 
 if __name__ == '__main__':
     # Хендлеры
     start_command_handler = CommandHandler('start', start)
-    text_message_handler = MessageHandler(Filters.text, answere)
-    audio_message_handler = MessageHandler(Filters.voice, audio)
+    text_message_handler = MessageHandler(Filters.text, answer)
+    audio_message_handler = MessageHandler(Filters.voice, voice)
+
     # Добавляем хендлеры в диспетчер
     dispatcher.add_handler(start_command_handler)
     dispatcher.add_handler(text_message_handler)
     dispatcher.add_handler(audio_message_handler)
+    dispatcher.add_handler(MessageHandler(Filters.document, document))
+    dispatcher.add_handler(MessageHandler(Filters.location, location))
+    dispatcher.add_handler(MessageHandler(Filters.photo, photo))
+    dispatcher.add_handler(MessageHandler(Filters.sticker, sticker))
 
     # Начинаем поиск обновлений
     updater.start_polling(clean=True)
---
+++ data/data.py
@@ -0,0 +1,128 @@
+EN_WHITELIST = '0123456789йцукенгшщзхъёфывапролджэячсмитьбю '  # space is included in whitelist
+EN_BLACKLIST = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~\''
+
+FILENAME = 'general.txt'
+
+limit = {
+    'maxq': 20,
+    'minq': 0,
+    'maxa': 20,
+    'mina': 3
+}
+
+# VOCAB_SIZE = 6000
+VOCAB_SIZE = 15000
+
+
+import numpy as np
+
+import pickle
+
+'''
+ read lines from file
+     return [list of lines]
+
+'''
+
+
+def read_lines(filename):
+    return open(filename).read().split('\n')[:-1]
+
+
+'''
+ split sentences in one line
+  into multiple lines
+    return [list of lines]
+
+'''
+
+
+def split_line(line):
+    return line.split('.')
+
+
+'''
+ remove anything that isn't in the vocabulary
+    return str(pure ta/en)
+
+'''
+
+
+def filter_line(line, whitelist):
+    return ''.join([ch for ch in line if ch in whitelist])
+
+
+'''
+ filter too long and too short sequences
+    return tuple( filtered_ta, filtered_en )
+
+'''
+
+
+def filter_data(sequences):
+    filtered_q, filtered_a = [], []
+    # raw_data_len = len(sequences) // 2
+
+    for i in range(0, len(sequences) - 1, 2):
+        qlen, alen = len(sequences[i].split(' ')), len(sequences[i + 1].split(' '))
+        if qlen >= limit['minq'] and qlen <= limit['maxq']:
+            if alen >= limit['mina'] and alen <= limit['maxa']:
+                filtered_q.append(sequences[i])
+                filtered_a.append(sequences[i + 1])
+
+    # print the fraction of the original data, filtered
+    # filt_data_len = len(filtered_q)
+    # filtered = int((raw_data_len - filt_data_len) * 100 / raw_data_len)
+    # print(str(filtered) + '% filtered from original data')
+
+    return filtered_q, filtered_a
+
+
+def clear_message(message):
+    message = message.lower()
+    message = filter_line(message, EN_WHITELIST)
+    return message.split()
+
+
+def process_data():
+    print('\n>> Read lines from file')
+    lines = read_lines(filename=FILENAME)
+
+    # change to lower case (just for en)
+    lines = [line.lower() for line in lines]
+
+    print('\n:: Sample from read(p) lines')
+    print(lines[121:125])
+
+    # filter out unnecessary characters
+    print('\n>> Filter lines')
+    lines = [filter_line(line, EN_WHITELIST) for line in lines]
+    print(lines[121:125])
+
+    # filter out too long or too short sequences
+    print('\n>> 2nd layer of filtering')
+    qlines, alines = filter_data(lines)
+    print('\nq : {0} ; a : {1}'.format(qlines[60], alines[60]))
+    print('\nq : {0} ; a : {1}'.format(qlines[61], alines[61]))
+
+    # convert list of [lines of text] into list of [list of words ]
+    print('\n>> Segment lines into words')
+    qtokenized = [wordlist.split(' ') for wordlist in qlines]
+    atokenized = [wordlist.split(' ') for wordlist in alines]
+    print('\n:: Sample from segmented list of words')
+    print('\nq : {0} ; a : {1}'.format(qtokenized[60], atokenized[60]))
+    print('\nq : {0} ; a : {1}'.format(qtokenized[61], atokenized[61]))
+
+    metadata = {
+        'q_tok': qtokenized,
+        'a_tok': atokenized,
+        'limit': limit,
+    }
+
+    # write to disk : data control dictionaries
+    with open('metadata_corpus.pkl', 'wb') as f:
+        pickle.dump(metadata, f)
+
+
+if __name__ == '__main__':
+    process_data()
